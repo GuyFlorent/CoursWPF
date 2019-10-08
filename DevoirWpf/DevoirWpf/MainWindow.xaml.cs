440d58c775a0c7d470e7f67df5fcc87662bf3dea
stagiaire <stagiaire@PC-B-8-2CT8V02>
2019-10-08 14:44:36 +0200

    ajout des derbnier boutons

--- DevoirWpf/DevoirWpf/MainWindow.xaml.cs
+++ DevoirWpf/DevoirWpf/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DevoirWpf
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
